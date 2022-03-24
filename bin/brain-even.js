c2d41f2f3c2941cd1c33cddc792e476a9249571c
alex <alexandra.griorchenko@gmail.com>
2022-03-24 12:46:52 +0700

    update brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 /* eslint-disable eol-last */
 import startIsEvenGame from '../src/cli-even.js';
 import greetingByUserName from '../src/cli.js';
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,3 @@
-#!/usr/bin/env node
 /* eslint-disable eol-last */
 
 import greetingByUserName from '../src/cli.js';
