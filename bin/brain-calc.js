0c378357b0d5115703286a5c1ee94083ac66ee47
alex <alexandra.griorchenko@gmail.com>
2022-03-24 18:51:19 +0700

    add shebang to brain-calc.js and brain-games.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import greetingByUserName from '../src/cli.js';
 
 greetingByUserName();
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,6 @@
-/* eslint-disable eol-last */
+#!/usr/bin/env node
 
+/* eslint-disable eol-last */
 import greetingByUserName from '../src/cli.js';
 
 greetingByUserName();
