9487ccfdd36ddc7aaf68eb4a50b0107d4e79a3bb
alex <alexandra.griorchenko@gmail.com>
2022-03-24 20:21:17 +0700

    add generateRandomChar func to cli-calc.js

--- src/cli-calc.js
+++ src/cli-calc.js
@@ -1,1 +1,9 @@
 import { generateRandomNumber } from './cli-even.js';
+import { askUser } from './cli-ask-answer.js';
+
+const generateRandomChar = () => {
+  const arr = ['+', '-', '*'];
+  const i = generateRandomNumber(0, 2);
+  const result = arr[i];
+  return result;
+};
