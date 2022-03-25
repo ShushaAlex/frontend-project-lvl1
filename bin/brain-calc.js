d320e8cbd3503d14c65dbb69deed78e70b767868
alex <alexandra.griorchenko@gmail.com>
2022-03-25 14:21:53 +0700

    import startCalcGame to brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
+/* eslint-disable eol-last */
 import greetingByUserName from '../src/cli.js';
+import startCalcGame from '../src/cli-calc.js';
 
 greetingByUserName();
+startCalcGame();
