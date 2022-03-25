b1d02e31a3ac0cc89a626f38ec87b7b2b4fae807
alex <alexandra.griorchenko@gmail.com>
2022-03-25 13:58:34 +0700

    add logic for brain-calc game in cli-calc.js

--- src/cli-calc.js
+++ src/cli-calc.js
@@ -1,9 +1,59 @@
+/* eslint-disable eol-last */
+import readlineSync from 'readline-sync';
 import { generateRandomNumber } from './cli-even.js';
 import { askUser } from './cli-ask-answer.js';
+import { userName } from './cli.js';
 
-const generateRandomChar = () => {
-  const arr = ['+', '-', '*'];
-  const i = generateRandomNumber(0, 2);
+const generateRandomChar = (arr) => {
+  const arrLength = arr.length();
+  const i = generateRandomNumber(0, arrLength - 1);
   const result = arr[i];
   return result;
 };
+
+const startRound = () => {
+  const firstNumber = generateRandomNumber(0, 10);
+  const secondNumber = generateRandomNumber(0, 10);
+  const operator = generateRandomChar(['+', '-', '*']);
+  console.log('What is the result of the expression?');
+  askUser(firstNumber, operator, secondNumber);
+
+  const calculateAnswer = () => {
+    switch (operator) {
+      case '+':
+        answer = firstNumber + secondNumber;
+        break;
+      case '-':
+        answer = firstNumber - secondNumber;
+        break;
+      case '*':
+        answer = firstNumber * secondNumber;
+        break;
+      default:
+        answer = 'There is no operator';
+        break;
+    }
+  };
+  const correctAnswer = calculateAnswer.toString();
+  const userAnswer = readlineSync.question('Your answer: ');
+  if (userAnswer === correctAnswer) {
+    console.log('Correct!');
+    return true;
+  }
+  console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
+  return false;
+};
+
+const startCalcGame = () => {
+  const numberOfRounds = 3;
+  for (let i = 0; i < numberOfRounds; i += 1) {
+    const result = startRound();
+    if (result === false) {
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export default startCalcGame;
