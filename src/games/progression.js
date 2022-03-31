f6f64efb6efe5c3df4d82a989eb218226d36cea6
alex <alexandra.griorchenko@gmail.com>
2022-03-31 16:45:14 +0700

    remake getProgression in progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,16 +3,19 @@ import { generateRandomNumber } from '../index.js';
 
 const getProgression = () => {
   const numbers = [];
-  const progressionStep = generateRandomNumber(0, 5);
+  const progressionStep = generateRandomNumber(2, 5);
   const progressionStart = generateRandomNumber(0, 50);
   const progressionNumbersCount = generateRandomNumber(5, 10);
-  const missedNumber = generateRandomNumber(0, progressionNumbersCount - 1);
   const progressionEnd = progressionStart + (progressionStep * progressionNumbersCount);
   for (let i = progressionStart; i <= progressionEnd; i += progressionStep) {
     numbers.push(i);
   }
-  numbers[missedNumber] = '..';
-  console.log(numbers.join(' '));
+  const missedNumberIndex = generateRandomNumber(0, numbers.length - 1);
+  const missedNumber = numbers[missedNumberIndex];
+  numbers[missedNumberIndex] = '..';
+  return [numbers.join(' '), missedNumber.toString()];
 };
 
-export default getProgression;
+const progressionFinal = getProgression();
+const task = progressionFinal[0];
+const correctAnswer = progressionFinal[1];
