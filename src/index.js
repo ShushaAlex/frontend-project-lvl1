e9ffb8b99330b43dc5ed0b00e850a3ed1959bf0a
alex <alexandra.griorchenko@gmail.com>
2022-04-02 19:14:20 +0700

    move generateRandomNumber to get-random-num.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 /* eslint-disable eol-last */
 // eslint-disable-next-line import/prefer-default-export
-export const generateRandomNumber = (min, max) => Math.round(Math.random() * (max - min) + min);
 
-export const compareAnswers = (one, two) => {
+const compareAnswers = (one, two) => {
   if (one === two) {
     return true;
   }
