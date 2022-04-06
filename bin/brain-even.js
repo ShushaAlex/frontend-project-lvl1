2ef8f00225ab7c5da329f10d39e3cc701d4ce2da
alex <alexandra.griorchenko@gmail.com>
2022-04-06 17:26:25 +0700

    rewrite brain even game

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 /* eslint-disable eol-last */
 import startIsEvenGame from '../src/games/even.js';
-import greetingByUserName from '../src/cli.js';
 
-greetingByUserName();
 startIsEvenGame();
--- src/games/even.js
+++ src/games/even.js
@@ -1,40 +1,17 @@
 /* eslint-disable eol-last */
-import readlineSync from 'readline-sync';
-import { userName } from '../cli.js';
 import generateRandomNumber from '../get-random-num.js';
+import startGame from '../index.js';
 
-const isEven = (num) => {
+const isEven = () => {
+  const num = generateRandomNumber(0, 20);
   if (num === 0 || num % 2 === 0) {
-    return 'yes';
+    return [num, 'yes'];
   }
-  return 'no';
+  return [num, 'no'];
 };
 
-const startRound = () => {
-  const randomNumber = generateRandomNumber(0, 100);
-  const correctAnswer = isEven(randomNumber);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  console.log(`Question: ${randomNumber}`);
-  const userAnswer = readlineSync.question('Your answer: ');
+const gameRules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-  if (userAnswer === correctAnswer) {
-    console.log('Correct!');
-    return true;
-  }
-  console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
-  return false;
-};
-
-const startIsEvenGame = () => {
-  const numberOfRounds = 3;
-  for (let i = 0; i < numberOfRounds; i += 1) {
-    const result = startRound();
-    if (result === false) {
-      console.log(`Let's try again, ${userName}!`);
-      return;
-    }
-  }
-  console.log(`Congratulations, ${userName}!`);
-};
+const startIsEvenGame = () => startGame(gameRules, isEven);
 
 export default startIsEvenGame;
