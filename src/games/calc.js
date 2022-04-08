8bed57dcaed3b8566156585c019d1e0394ae7c92
alex <alexandra.griorchenko@gmail.com>
2022-04-08 15:55:39 +0700

    update calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -3,34 +3,34 @@ import generateRandomNumber from '../getRandomNum.js';
 import engineGame from '../index.js';
 
 const generateRandomChar = (arr) => {
-  const i = generateRandomNumber(0, 2);
+  const i = generateRandomNumber(0, arr.length - 1);
   const result = arr[i];
   return result;
 };
 
-const calcGame = () => {
+const calculateAnswer = (str, num1, num2) => {
+  switch (str) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    default:
+      return 'There is no operator';
+  }
+};
+
+const generateData = () => {
   const firstNumber = generateRandomNumber(0, 10);
   const secondNumber = generateRandomNumber(0, 10);
   const operator = generateRandomChar(['+', '-', '*']);
   const task = `${firstNumber} ${operator} ${secondNumber}`;
-
-  const calculateAnswer = (str) => {
-    switch (str) {
-      case '+':
-        return firstNumber + secondNumber;
-      case '-':
-        return firstNumber - secondNumber;
-      case '*':
-        return firstNumber * secondNumber;
-      default:
-        return 'There is no operator';
-    }
-  };
-  const correctAnswer = calculateAnswer(operator).toString();
+  const correctAnswer = calculateAnswer(operator, firstNumber, secondNumber).toString();
   return [task, correctAnswer];
 };
 
 const gameRules = 'What is the result of the expression?';
-const startCalcGame = () => engineGame(gameRules, calcGame);
+const startCalcGame = () => engineGame(gameRules, generateData);
 
 export default startCalcGame;
