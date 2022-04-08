8e23da8fed73e1ecf3f11b913c719ecd3c65ed5a
alex <alexandra.griorchenko@gmail.com>
2022-04-08 16:12:21 +0700

    update isEven.js

--- src/games/even.js
+++ src/games/even.js
@@ -2,16 +2,19 @@
 import generateRandomNumber from '../getRandomNum.js';
 import engineGame from '../index.js';
 
-const isEven = () => {
+const isEven = (num) => {
+  const result = !!((num === 0 || num % 2 === 0));
+  return result;
+};
+
+const generateData = () => {
   const num = generateRandomNumber(0, 20);
-  if (num === 0 || num % 2 === 0) {
-    return [num, 'yes'];
-  }
-  return [num, 'no'];
+  const result = isEven(num) ? 'yes' : 'no';
+  return [num, result];
 };
 
 const gameRules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const startIsEvenGame = () => engineGame(gameRules, isEven);
+const startIsEvenGame = () => engineGame(gameRules, generateData);
 
 export default startIsEvenGame;
