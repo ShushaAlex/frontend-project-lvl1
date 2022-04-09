b7623347e200f94f9ac19c0f438aaadfe4d55c19
alex <alexandra.griorchenko@gmail.com>
2022-04-09 12:43:48 +0700

    update gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -2,36 +2,19 @@
 import generateRandomNumber from '../getRandomNum.js';
 import engineGame from '../index.js';
 
-const getDividers = (num) => {
-  const dividers = [];
-  for (let i = 1; i <= num; i += 1) {
-    if (num % i === 0) {
-      dividers.push(i);
-    }
+const gcd = (num1, num2) => {
+  let i = num1;
+  while (num1 % i !== 0 || num2 % i !== 0) {
+    i -= 1;
   }
-  return dividers;
-};
-const getCommonDivider = (arr1, arr2) => {
-  const newArray = [];
-  for (const item of arr1) {
-    if (arr2.includes(item)) {
-      newArray.push(item);
-    }
-  }
-  return newArray[newArray.length - 1];
-};
-
-const calCorrectAnswer = (num1, num2) => {
-  const dividersOfFirstNum = getDividers(num1);
-  const dividersOfSecondNum = getDividers(num2);
-  return getCommonDivider(dividersOfFirstNum, dividersOfSecondNum);
+  return i;
 };
 
 const generateData = () => {
   const firstNumber = generateRandomNumber(0, 100);
   const secondNumber = generateRandomNumber(0, 100);
   const task = `${firstNumber} ${secondNumber}`;
-  const correctAnswer = calCorrectAnswer(firstNumber, secondNumber).toString();
+  const correctAnswer = gcd(firstNumber, secondNumber).toString();
   return [task, correctAnswer];
 };
 
