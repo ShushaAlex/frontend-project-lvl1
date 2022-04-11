94dc0d0aab183947462f1ced205446eb435e5e3d
alex <alexandra.griorchenko@gmail.com>
2022-04-11 16:18:14 +0700

    update gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -3,20 +3,15 @@ import generateRandomNumber from '../getRandomNum.js';
 import engineGame from '../index.js';
 
 const gcd = (num1, num2) => {
-  let result;
-  if (num1 === num2) {
-    result = num1;
-  } else if (num1 > num2) {
-    result = gcd((num1 - num2), num2);
-  } else if (num1 < num2) {
-    result = gcd(num1, (num2 - num1));
+  if (!num2) {
+    return num1;
   }
-  return result;
+  return gcd(num2, num1 % num2);
 };
 
 const generateData = () => {
-  const firstNumber = generateRandomNumber(0, 100);
-  const secondNumber = generateRandomNumber(0, 100);
+  const firstNumber = generateRandomNumber(0, 20);
+  const secondNumber = generateRandomNumber(0, 20);
   const task = `${firstNumber} ${secondNumber}`;
   const correctAnswer = gcd(firstNumber, secondNumber).toString();
   return [task, correctAnswer];
